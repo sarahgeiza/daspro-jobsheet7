2272e73d5003aa146a93d95b8979a2dce5a08242
sarahgeiza <sarahgeizask@gmail.com>
2025-10-23 17:28:52 +0700

    modifikasi soal no 1

--- SiakadFor26.java
+++ SiakadFor26.java
@@ -5,6 +5,8 @@ public class SiakadFor26 {
         Scanner sc = new Scanner(System.in);
         
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidakLulus = 0;
+        
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
             nilai = sc.nextDouble();
@@ -14,8 +16,15 @@ public class SiakadFor26 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai >= 60) {
+                lulus++;
+            } else {
+                tidakLulus++;
+            }
         }
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println(" Jumlah mahasiswa lulus: "+ lulus);
+        System.out.println("Jumlah mahasiswa tidak lulus: " + tidakLulus);
     }
 }
